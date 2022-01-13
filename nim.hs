52b6fb70cf732e7dbf26c92cafb57700eac7c006
Tiago Carvalho <Tiago.Carvalho@thomsonreuters.com>
2022-01-12 21:31:24 -0300

    Jogada do usuário funcionando

--- nim.hs
+++ nim.hs
@@ -1,18 +1,21 @@
+{-# LANGUAGE BlockArguments #-}
 module Main where
-
+import qualified Data.Sequence as Seq
+import qualified Data.Foldable as Fol
 import Data.Char ()
 import Lib ()
 import Data.Array.MArray.Safe (mapIndices)
+import Data.Maybe
 
 data Jogador = Usuario | Computador deriving (Show, Eq)
 data Dificuldade = Facil | Dificil deriving (Show, Eq)
 
-tabuleiro :: [Int]
-tabuleiro = [1, 3, 5, 7]
+tabuleiro :: Seq.Seq Int
+tabuleiro = Seq.fromList [1, 3, 5, 7]
 
-jogo :: [Int] -> Jogador -> IO ()
+jogo :: Seq.Seq Int -> Jogador -> IO ()
 jogo tabuleiro jogador =
-    if tabuleiro == [0,0,0,0] then do
+    if tabuleiro == Seq.fromList [0,0,0,0] then
         putStrLn (show jogador ++ " venceu!")
     else do
         putStrLn ("Vez do " ++ show jogador)
@@ -22,42 +25,52 @@ jogo tabuleiro jogador =
             fileira <- getLine
             putStrLn "\nSelecione o numero de palitos a serem removidos: "
             numPalitosRemovidos <- getLine
-            --usuarioRemovePalitos()
+            let x = (read fileira :: Int)
+            let y = (read numPalitosRemovidos :: Int)
+            let tabuleiroAtualizado = jogadaUsuario tabuleiro (x-1) y
+            jogo tabuleiroAtualizado (alterna jogador)
+        else
             mostraTabuleiro tabuleiro
-        else do
-            --jogadaComputador
-            mostraTabuleiro tabuleiro        
-        jogo tabuleiro (alterna jogador)
 
--- removePalitos :: [Int] -> Int
--- removePalitos tabuleiro fileira qtdePalitos =
---   if (jogadaValida tabuleiro fileira qtdePalitos) then do
-                                                  
--- jogadaValida :: [Int] -> Int -> Int -> Bool 
--- jogadaValida tabuleiro fileira qtdePalitos = if (tabuleiro !! (fileira-1)) >= qtdePalitos
---                                                  then True 
---                                              else False
+
+jogadaUsuario :: Seq.Seq Int -> Int -> Int -> Seq.Seq Int
+jogadaUsuario tabuleiro fileira palitos =
+    if jogadaValida tabuleiro fileira palitos then
+        removePalitos tabuleiro fileira palitos
+    else tabuleiro
+
+
+removePalitos :: Seq.Seq Int -> Int -> Int -> Seq.Seq Int
+removePalitos tabuleiro fileira qtdePalitos =
+    Seq.adjust (\x -> x - qtdePalitos) fileira tabuleiro
+
+
+jogadaValida :: Seq.Seq Int -> Int -> Int -> Bool
+jogadaValida tabuleiro fileira qtdePalitos =
+    Seq.index tabuleiro fileira >= qtdePalitos && fileira < 4 && fileira >= 0
+
 
 alterna :: Jogador -> Jogador
 alterna Usuario = Computador
 alterna Computador = Usuario
 
-mostraTabuleiro :: [Int] -> IO ()
-mostraTabuleiro tabuleiro = do putStrLn ("1: " ++ replicate (tabuleiro !! 0) 'i')
-                               putStrLn ("2: " ++ replicate (tabuleiro !! 1) 'i')
-                               putStrLn ("3: " ++ replicate (tabuleiro !! 2) 'i')
-                               putStrLn ("4: " ++ replicate (tabuleiro !! 3) 'i')
+
+mostraTabuleiro :: Seq.Seq Int -> IO ()
+mostraTabuleiro tabuleiro = do putStrLn ("1: " ++ replicate (head (Fol.toList tabuleiro)) 'i')
+                               putStrLn ("2: " ++ replicate (Fol.toList tabuleiro !! 1) 'i')
+                               putStrLn ("3: " ++ replicate (Fol.toList tabuleiro !! 2) 'i')
+                               putStrLn ("4: " ++ replicate (Fol.toList tabuleiro !! 3) 'i')
 
 main :: IO ()
 main = do
     putStrLn "Niveis de dificuldade:"
     putStrLn "0: Facil:\n1: Dificil"
     putStr "Selecione um nivel: "
-    dificuldade <- readLn
-    if dificuldade == Facil then do
+    dificuldade <- getLine
+    if dificuldade == "0" then do
         putStrLn "Iniciando jogo facil!"
         jogo tabuleiro Usuario
-    else if dificuldade == Dificil then do
+    else if dificuldade == "1" then do
         putStrLn "Iniciando jogo dificil!"
         jogo tabuleiro Computador
     else do
